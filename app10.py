a8a9cfcdf314ea3102adfec8b881842fc023404b
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-01-19 10:31:36 -0300

    updades

---
+++ app10.py
@@ -0,0 +1,8 @@
+# Lists:
+
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Maria']
+print(names[2:])
+print(names)
+#We can modify any element of this list
+names[0] = 'Jon'
+print(names)
--- ex9.py
+++ ex9.py
@@ -4,4 +4,3 @@ for x_count in numbers:
    for count in range(x_count):
        output += 'x'
    print(output)
-#1:55:59
