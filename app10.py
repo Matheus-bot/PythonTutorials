0a11ae518320f2a71c5fe1bc4023b5bb404b8cb3
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-01-20 10:32:38 -0300

    updades

--- app10.py
+++ app10.py
@@ -1,8 +1,28 @@
 # Lists:
-
+'''
 names = ['John', 'Bob', 'Mosh', 'Sarah', 'Maria']
 print(names[2:])
 print(names)
 #We can modify any element of this list
 names[0] = 'Jon'
-print(names)
+print(names)'''
+
+# 2D list
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+'''
+matrix[0][1] = 20 
+print(matrix[0][1])'''
+'''for row in matrix:
+    for item in row:
+        print(item)'''
+
+
+
+
+
+
+
---
+++ app11.py
@@ -0,0 +1,21 @@
+# List Methods
+
+numbers = [5, 2, 1, 5, 7, 4]
+'''numbers.append(20)
+numbers.insert(0, 10)
+numbers.remove(5)
+numbers.clear()
+numbers.pop()
+numbers.index()
+print(numbers)
+print(5 in numbers)
+numbers.sort()
+numbers.reverse()
+print(numbers)'''
+
+numbers2 = numbers.copy()
+numbers.append(10)
+
+print(numbers2)
+print(numbers)
+
