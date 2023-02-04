6e2fde6c35d14eccce1996b791cdd0071efc1255
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-02-04 10:27:27 -0300

    upp

---
+++ app12.py
@@ -0,0 +1,31 @@
+# Tuples
+#tuples are imutable
+'''numbers = (1, 2, 3)
+print(numbers[0])
+'''
+# unpacking
+'''
+coordinates = (1, 2, 3)
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+
+x, y, z = coordinates
+print(3)
+'''
+
+# dictionaries
+'''
+custumer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+}
+
+print(custumer.get("age"))'''
+
+
+
+
+
+
