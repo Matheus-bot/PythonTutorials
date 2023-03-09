9f1e9a90380bbcab3e931246e998d0c4466c1aed
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-03-09 10:19:30 -0300

    upp

--- app10.py
+++ app10.py
@@ -1,21 +1,21 @@
 # Lists:
 '''
 names = ['John', 'Bob', 'Mosh', 'Sarah', 'Maria']
-print(names[2:])
+print(names[:])
 print(names)
 #We can modify any element of this list
 names[0] = 'Jon'
 print(names)'''
 
 # 2D list
-matrix = [
+'''matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-'''
-matrix[0][1] = 20 
-print(matrix[0][1])'''
+matrix[1][2] = 20
+print(matrix[1][2])'''
+
 '''for row in matrix:
     for item in row:
         print(item)'''
