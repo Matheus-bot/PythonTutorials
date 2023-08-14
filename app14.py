fab8cec2f40ef4afd3051401d4e4fbed604e15a9
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-08-14 10:59:58 -0300

    update

---
+++ app14.py
@@ -0,0 +1,11 @@
+# Functions, parametres, keyword arguments
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name}{last_name}!')
+    print('Welcome aboard')
+
+
+print('Start')
+greet_user(last_name='Smith', first_name='John ')
+print("Finish")
+
+#2:40:24
