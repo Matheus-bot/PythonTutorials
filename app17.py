f6ccf5ebf6b29959a891f8130989fdb4a6667f22
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2023-08-16 09:28:25 -0300

    update

--- app17.py
+++ app17.py
@@ -1,1 +1,15 @@
 # comments to explains Whys and Hows NOT what's
+# Classes -> used to define new types
+
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+
+
+
